ce4ee3881746c91e67fd29364e68d0fddf4055d3
jhoan <johanmonsalve125@gmail.com>
2024-10-27 12:43:30 -0500

    gestorcontrasenas44444

--- src/app/api/items/[itemid]/route.ts
+++ src/app/api/items/[itemid]/route.ts
@@ -1,31 +1,33 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
-export async function PATCH(req: Request, { params }: { params: { itemid: string } }) {
-    try {
-        const { itemid } = params;
-        const values = await req.json();
+export async function PATCH(
+  req: Request,
+  { params }: { params: { itemid: string } }
+) {
+  try {
+    const { itemid } = params;
+    const values = await req.json();
 
-        if (!itemid) {
-            return new NextResponse("unauthorized", { status: 401 });
-        }
-
-        // Evitar que userid se pase en los valores para actualizar
-        const { userId, ...updateValues } = values;
-
-        const element = await db.element.update({
-            where: {
-                id: itemid,
-            },
-            data: {
-                ...updateValues, // Se actualizan solo los campos permitidos
-            },
-        });
-
-        return new NextResponse(JSON.stringify(element), { status: 200 });
-
-    } catch (error) {
-        console.error(error);
-        return new NextResponse("Internal Error", { status: 500 });
+    if (!itemid) {
+      return new NextResponse("unauthorized", { status: 401 });
     }
+
+    // Rename userId to _userId to avoid the unused variable warning
+    const { userId: _userId, ...updateValues } = values;
+
+    const element = await db.element.update({
+      where: {
+        id: itemid,
+      },
+      data: {
+        ...updateValues, // Only the allowed fields are updated
+      },
+    });
+
+    return new NextResponse(JSON.stringify(element), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
