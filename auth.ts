d72b0fa87b2f0231aeccfb67b82a011909fb9c99
jhoan <johanmonsalve125@gmail.com>
2024-10-27 21:12:08 -0500

    gestor151515

--- auth.ts
+++ auth.ts
@@ -6,7 +6,21 @@ import authConfig from "./auth.config"
 
 const prisma = new PrismaClient()
 export const {handlers, signIn, signOut, auth} = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    ...authConfig,
-    session: { strategy: "jwt" },
+  adapter: PrismaAdapter(prisma),
+  ...authConfig,
+  session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token }) {
+      console.log("JWT Callback Token:", token);
+      return token;
+    },
+    async session({ session, token }) {
+      console.log("Session Callback Token:", token);
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+    
 })
